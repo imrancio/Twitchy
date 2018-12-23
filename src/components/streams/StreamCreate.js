2d9b272663c276930db92c05d6826a0aa5902821
Imran Chowdhury <imran@imranc.io>
2018-12-24 08:09:09 +1100

    Added input validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         {/* ES6 syntax - takes all key/val pairs and adds them as props */}
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
@@ -16,13 +17,13 @@ class StreamCreate extends React.Component {
     console.log(formValues);
   }
 
-  // Field passes additional props to component
   render() {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
         className="ui form"
       >
+        {/* Field passes additional props to component */}
         <Field name="title" component={this.renderInput} label="Enter title" />
         <Field
           name="description"
@@ -35,6 +36,20 @@ class StreamCreate extends React.Component {
   }
 }
 
+const validate = formValues => {
+  const errors = {};
+  // invalid form
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  // valid form returns empty object
+  return errors;
+};
+
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate
 })(StreamCreate);
