5cb3c3902c75c3a849b3f01148c6e1ef591c4bae
Imran Chowdhury <imran@imranc.io>
2018-12-24 07:49:38 +1100

    Set up StreamCreate form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,16 +2,26 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input }) {
-    // ES6 syntax - takes all key/val pairs and adds them as props
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        {/* ES6 syntax - takes all key/val pairs and adds them as props */}
+        <input {...input} />
+      </div>
+    );
   }
 
+  // Field passes additional props to component
   render() {
     return (
-      <form action="">
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
       </form>
     );
   }
