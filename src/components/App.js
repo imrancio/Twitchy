285d57a73396ae07b086aa80a87d9abed4e5298e
Imran Chowdhury <imran@imranc.io>
2018-12-24 02:02:47 +1100

    Added routes & boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-// <Link> replaces <a> and keeps state on new route
-// URL still changes but does not request new HTML
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/2">Navigate to 2</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-      <Link to="/">Navigate to 1</Link>
-    </div>
-  );
-};
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/2" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
