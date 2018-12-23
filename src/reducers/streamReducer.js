1796f3a5176fd374e977ca6b00c16c2be61981a4
Imran Chowdhury <imran@imranc.io>
2018-12-24 10:13:42 +1100

    Connected streams reducer with REST actions

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,30 @@
+import _ from "lodash";
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      // mapKeys maps list to object - id is key for each list object
+      // ES6 syntax - ... gets all key/value pairs out of object
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case FETCH_STREAM:
+      // ES6 syntax - copy object with new key/value pair
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      // Redux always gets new state object
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      // payload is stream id to be deleted, omit returns new object
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
