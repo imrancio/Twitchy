65296e1bb9e2b2c21e69bfab1228a53a00c235e3
Imran Chowdhury <imran@imranc.io>
2018-12-24 11:06:00 +1100

    Conditional render of admin buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,11 +7,23 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       // List elements should have unique key prop
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -23,7 +35,6 @@ class StreamList extends React.Component {
   }
 
   render() {
-    console.log(this.props.streams);
     return (
       <div>
         <h2>Streams</h2>
@@ -35,7 +46,10 @@ class StreamList extends React.Component {
 
 const mapStateToProps = state => {
   // turn objects to lists for component - easier handling
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(
