5d20863509a170e9b91c9a803a4af2053ed42914
Imran Chowdhury <imran@imranc.io>
2018-12-24 11:57:33 +1100

    Refactored/connected StreamEdit Route

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -33,6 +34,7 @@ export const createStream = formValues => async (dispatch, getState) => {
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
   // Programmatic navigation to return user to root route
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
