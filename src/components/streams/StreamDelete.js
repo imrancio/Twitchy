bbd6d71871fcf0c81270de7b2c5d0864bf2efcce
Imran Chowdhury <imran@imranc.io>
2018-12-24 14:55:29 +1100

    DeleteStream fetches deletion stream, customises text

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,26 +1,51 @@
-import Modal from "../Modal";
 import React from "react";
+import { connect } from "react-redux";
+import Modal from "../Modal";
 import history from "../../history";
+import { fetchStream } from "../../actions";
 
-const StreamDelete = () => {
-  const actions = (
-    // React.Fragments are invisible and not rendered to DOM - can be shortened to <></>
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      StreamDelete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    return (
+      // React.Fragments are invisible and not rendered to DOM - can be shortened to <></>
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+
+    return `Are you sure you want to delete the stream titled "${
+      this.props.stream.title
+    }"`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
         onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamDelete);
